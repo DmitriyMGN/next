50e28a6770e4e5af1cc50924c18a42fc6196fb22
Dmitriy Mysiagin <dmitry.mysyagin@yandex.ru>
2023-11-29 22:03:32 +0300

    tsc1

---
+++ test.ts
@@ -0,0 +1,42 @@
+let a: number = 4;
+let b = "ddddd";
+let c = true;
+
+let d: string[] = ["sds", 'ddsd'];
+
+let e: any = 3;
+e = "ds";
+
+const test = (a: string): number | string => {
+  return '';
+}
+
+const test2 = (a: number): number => {
+  return a*2;
+}
+
+d = d.map((x: string) => x.toLocaleLowerCase())
+
+const countCoord = (coord: { lat: number, long?: number}) => {
+  return coord.lat 
+}
+
+console.log(countCoord({lat: 1}))
+
+
+const  printIr = (id: number | string) => {
+  if (typeof id === 'number') {
+    console.log(id.toString)
+  } else if (typeof id === 'string') {
+    console.log(id.toLocaleUpperCase)
+  }
+}
+
+const getSum = (a: number | number[]) => {
+  if (Array.isArray(a)) {
+   
+  } 
+}
+
+const x: undefined = undefined;
+const z: null = null;
