93b5244c0851cd668be4c23f59fd1e0801a1590f
Dmitriy Mysiagin <dmitry.mysyagin@yandex.ru>
2023-12-06 17:46:13 +0300

    P and Tag components

--- top-app/app/page.tsx
+++ top-app/app/page.tsx
@@ -1,12 +1,20 @@
 // import styles from './page.module.css';
-import { Button, Htag } from '@/components';
+import { Button, Htag, P, Tag } from '@/components';
 
 export default function Home(): JSX.Element {
 	return (
 		<main>
 			<Htag tag='h1' className='dsad'>Текст</Htag>
-			<Button appearance='primary' className='dsad'>Текст</Button>
-			<Button appearance='ghost'>Текстsssssssssssss</Button>
+			<Button appearance='primary' className='dsad' arrow='down'>Текст</Button>
+			<Button appearance='ghost' arrow='right'>Текстsssssssssssss</Button>
+			<P size='m'>Маленький</P>
+			<P>Средний</P>
+			<P size='b'>Большой</P>
+			<Tag size='b'>Ghost</Tag>
+			<Tag size='s' color='red'>Red</Tag>
+			<Tag size='s' color='primary'>Primary</Tag>
+			<Tag size='s' color='gray'>Gray</Tag>
+			<Tag size='b' color='green' href="www.ya.ru">Green</Tag>
 		</main>
 	);
 }
