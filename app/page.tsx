004349d946e20cd66a1d7b1e1a755fe0809a6efb
Dmitriy Mysiagin <dmitry.mysyagin@yandex.ru>
2023-12-07 13:32:00 +0300

    first review

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-// import styles from './page.module.css';
 import { Card } from '@/components';
 
 export default function Home(): JSX.Element {
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,5 +1,7 @@
 import { ImageComponent, Like, Paragraph, Tag, Time, Title, Dot } from '..';
+import Link from 'next/link';
 import styles from './Card.module.css';
+import ArrowImg from '../../public/arrow.svg';
 
 export const Card = (): JSX.Element => {
 	return (
@@ -11,13 +13,17 @@ export const Card = (): JSX.Element => {
 					<Dot />
 					<Time>1 месяц назад</Time>
 				</div>
-
 				<Like>4</Like>
-
 			</div>
-
 			<Title tag="h3">Как работать с CSS Grid</Title>
 			<Paragraph size='s'>Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы..</Paragraph>
+			<div className={styles.bottomContainer}>
+				<Time>3 минуты</Time>
+				<Link href="#" className={styles.link}>
+					Читать
+					<ArrowImg />
+				</Link>
+			</div>
 		</div>
 	);
 };
