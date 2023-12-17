5c9afdfefc6c11019c40d4fb0d1f5f04e1584a08
Dmitriy Mysiagin <dmitry.mysyagin@yandex.ru>
2023-12-17 17:49:15 +0300

    fetch

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,26 @@
 // import styles from './page.module.css';
 
-export default function Home(): JSX.Element {
+// async function getMenu(firstCategory: number) {
+// 	const res = await fetch('https://courses-top.ru', {
+// 		method: 'POST',
+// 		body: JSON.stringify({
+// 			firstCategory: firstCategory
+// 		}),
+// 		headers: new Headers({ 'content-type': 'application/json' })
+// 	});
+// 	console.log(res.json());
+// 	return res.json();
+// }
 
+export default async function Home() {
+	// const menu = await getMenu(1);
 	return (
 		<main>
 			Главная страница
+			<div>
+				{/* <div>{JSON.stringify(menu)}</div>
+				<div>{menu.length}</div> */}
+			</div>
 		</main>
 	);
 }
