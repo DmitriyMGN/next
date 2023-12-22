c1467f3a9983a2c437f67788baa722b8dd8a24d5
Dmitriy Mysiagin <dmitry.mysyagin@yandex.ru>
2023-12-22 08:20:27 +0300

    change post component

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,20 +1,37 @@
+interface ICard {
+	id: number;
+	userId: number;
+	title: string;
+	body: string;
+}
+
+const getPost = async (id: number) => {
+	const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+		.then((res) => {
+			if (res.ok) {
+				return res.json();
+			}
+			return Promise.reject(`Ошибка: ${res.status}`);
+		})
+		.then((data) => {
+			return data;
+		});
+
+	return result;
+};
+
+export async function generateStaticParams() {
+	const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json());
+
+	return posts.map((post: ICard) => ({
+		id: post.id.toString(),
+	}));
+}
+
+
 export default async function Post({ params }: { params: { id: number } }): Promise<JSX.Element> {
-	const getPost = async () => {
-		const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
-			.then((res) => {
-				if (res.ok) {
-					return res.json();
-				}
-				return Promise.reject(`Ошибка: ${res.status}`);
-			})
-			.then((data) => {
-				return data;
-			});
 
-		return result;
-	};
-
-	const post = await getPost();
+	const post = await getPost(params.id);
 
 	return (
 		<div>
