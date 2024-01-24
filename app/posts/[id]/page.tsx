e576998b1838caefa3e105ac11f3926c8f76602c
Dmitriy Mysiagin <dmitry.mysyagin@yandex.ru>
2024-01-24 12:03:36 +0300

    tested

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { AboutCard } from '@/components';
+import styles from './page.module.css';
+
 interface ICard {
-	id: number;
+	id: string;
 	userId: number;
 	title: string;
 	body: string;
@@ -20,11 +23,26 @@ const getPost = async (id: number) => {
 	return result;
 };
 
+const getComment = async (id: number) => {
+	const result = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
+		.then((res) => {
+			if (res.ok) {
+				return res.json();
+			}
+			return Promise.reject(`Ошибка: ${res.status}`);
+		})
+		.then((data) => {
+			return data;
+		});
+
+	return result;
+};
+
 export async function generateStaticParams() {
 	const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json());
 
 	return posts.map((post: ICard) => ({
-		id: post.id.toString(),
+		id: post.id.toString()
 	}));
 }
 
